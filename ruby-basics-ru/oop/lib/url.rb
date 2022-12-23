2f17deb3fe3335fdec23641417d68f2612a34d15
@hexlet/cli <support@hexlet.io>
2022-12-23 20:01:56 +0300

    submit ruby-basics-ru/oop

---
+++ ruby-basics-ru/oop/lib/url.rb
@@ -0,0 +1,47 @@
+# frozen_string_literal: true
+
+require 'uri'
+require 'forwardable'
+
+# BEGIN
+class Url
+  extend Forwardable
+  include Comparable
+  attr_accessor :url
+
+  def initialize(current_url)
+    @uri_service = URI(current_url)
+    @url = current_url
+    @params = nil
+  end
+
+  def ==(other)
+    @url == other.url
+  end
+
+  def query_params
+    return @params if @params
+
+    string_params = @uri_service.query
+    @params = {}
+
+    return @params unless string_params
+
+    result = []
+
+    string_params.split('&').each do |item|
+      result << item.split('=')
+    end
+    @params = Hash[result].transform_keys(&:to_sym)
+  end
+
+  def query_param(key, default = nil)
+    cur_params = query_params
+    return default unless query_params[key]
+
+    cur_params[key]
+  end
+
+  def_delegators :@uri_service, :scheme, :host
+end
+# END
