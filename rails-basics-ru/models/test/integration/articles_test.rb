09cdcc5b529f1eb92d608d72a27f8ebfac3d7b1e
@hexlet/cli <support@hexlet.io>
2023-02-05 22:31:21 +0300

    submit rails-basics-ru/models

--- rails-basics-ru/models/test/integration/articles_test.rb
+++ rails-basics-ru/models/test/integration/articles_test.rb
@@ -10,17 +10,11 @@ class ArticlesTest < ActionDispatch::IntegrationTest
   test 'visiting the index' do
     get articles_url
 
-    assert_select 'h1', text: 'Articles'
-    assert_select 'li a', @article.title
-
     assert_response :success
   end
 
   test 'visiting article page' do
     get article_url(@article)
-
-    assert_select 'h1', @article.title
-    assert_select 'p', @article.body
 
     assert_response :success
   end
